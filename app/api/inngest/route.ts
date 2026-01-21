505755ed6c624bdcc7ea69ec23ed86bcdc51e8a4
Tushar Gupta <t.gupta0202@gmail.com>
2026-01-21 21:41:48 +0530

    Implemented Daily News Summary and Mailing

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { inngest } from '@/lib/inngest/client'
-import { sendSignUPEmail } from '@/lib/inngest/functions'
+import { sendDailyNewsSummary, sendSignUpEmail } from '@/lib/inngest/functions'
 import { serve } from 'inngest/next'
 
 export const {GET, POST, PUT} = serve({
     client: inngest,
-    functions : [sendSignUPEmail],
+    functions : [sendSignUpEmail, sendDailyNewsSummary],
 })
